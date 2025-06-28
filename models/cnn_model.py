a821f377330f605b733a039eb3c43f1c5f2f613c
Trupal Mendpara <trupalmendpara@gmail.com>
2025-06-28 10:39:07 +0530

    Fixing the errors commit

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -4,11 +4,12 @@ import os
 import traceback
 import streamlit as st
 
-model_path = os.path.join(os.path.dirname(__file__), '..', 'disease_classifier.h5')
-model = tf.keras.models.load_model(model_path)
+
 class_names = ['COVID-19', 'Invalid', 'Normal', 'Pneumonia', 'Tuberculosis']
 
 def predict(image_array):
+    model_path = os.path.join(os.path.dirname(__file__), '..', 'disease_classifier.h5')
+    model = tf.keras.models.load_model(model_path)
     image_array = np.expand_dims(image_array, axis=0)  # Batch dimension
     prediction = model.predict(image_array)
     class_idx = np.argmax(prediction, axis=1)[0]
