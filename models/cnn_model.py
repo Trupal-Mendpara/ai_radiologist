0ddeec547e6396439459e41c90a1b6516f154b1d
Trupal Mendpara <trupalmendpara@gmail.com>
2025-06-28 10:37:49 +0530

    Fixing the errors commit

--- models/cnn_model.py
+++ models/cnn_model.py
@@ -4,17 +4,11 @@ import os
 import traceback
 import streamlit as st
 
+model_path = os.path.join(os.path.dirname(__file__), '..', 'disease_classifier.h5')
+model = tf.keras.models.load_model(model_path)
 class_names = ['COVID-19', 'Invalid', 'Normal', 'Pneumonia', 'Tuberculosis']
-@st.cache_resource(show_spinner="Loading model...")
-def load_model():
-    model_path = os.path.join(os.path.dirname(__file__), '..', 'disease_classifier.h5')
-    if not os.path.exists(model_path):
-        raise FileNotFoundError(f"Model file not found at {model_path}")
-    return tf.keras.models.load_model(model_path)
 
 def predict(image_array):
-    if not hasattr(predict, "model"):
-        predict.model = load_model()
     image_array = np.expand_dims(image_array, axis=0)  # Batch dimension
     prediction = model.predict(image_array)
     class_idx = np.argmax(prediction, axis=1)[0]
